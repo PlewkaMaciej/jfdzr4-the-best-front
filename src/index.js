773e4b60a9faf12bba044bccba19ff2ed4396a3b
Piotr <p.skolorzynski@gmail.com>
2021-09-21 12:58:28 +0200

    modify package.json by installing firebase package 	create firebaseConfiguration.js file to store firebase app configuration 	modify index.js file initialize firebase app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './normalize.css';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './firebaseConfiguration';
 import App from './App';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>

01c0d0bfd78709ff1066075f829f44d4e206791d
Maciej Plewka <plewka-maciej@wp.pl>
2021-10-03 14:35:46 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import CreatePost from "./components/postSection/createPost";
 import ShowPosts from "./components/postSection/showPosts";
+import SignUpForm from "./SignUpForm"
 function App() {
   return (
     <>
-      <CreatePost />
-      <ShowPosts />
+   
+      <SignUpForm/>
     </>
   );
 }

644d7e8a627a5ba5ea84e67ed4b17550f867cd60
Piotr <p.skolorzynski@gmail.com>
2021-10-05 22:34:42 +0200

    modify BookDetails component fetch data from firestore to get data  about exact document representing book

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../index';
 
 const BookDetails = () => {
-    const { id } = useParams;
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const docRef = doc(db, 'books', id);
+        getDoc(docRef)
+            .then(docSnap => {
+                if (docSnap.exists()) {
+                    console.log(docSnap.data());
+                }
+                setData(docSnap.data());
+            })
+    }, [id]);
     
     return (
-        <h2>Book Details - {id}</h2>
+        <h2 style={{marginTop: '10%'}}>{ data && data.title}</h2>
     );
 }
  

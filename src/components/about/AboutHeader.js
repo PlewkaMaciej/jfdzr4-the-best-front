b4d43c184e11141da0130e0f4bb64eeb7287e937
Emilia Falkowska <emilia.v.falkowska@gmail.com>
2021-11-01 20:34:36 +0100

    BF-21, BF-33, BF-34 AboutUs page and price layout

---
+++ src/components/about/AboutHeader.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { textAlign } from '@mui/system';
+
+export default function AboutHeader() {
+  return (
+    <Box sx={{ width: '100%', textAlign: 'center' }}>
+      <Typography variant="h4" gutterBottom component="div">
+        About SMART BOOKS.com
+      </Typography>
+       <Typography variant="subtitle1" gutterBottom component="div">
+       Smart Books.com offers online customers the Web's premier destination for books, eBooks, magazines and related products and services
+      </Typography>
+    </Box>
+  );
+}
